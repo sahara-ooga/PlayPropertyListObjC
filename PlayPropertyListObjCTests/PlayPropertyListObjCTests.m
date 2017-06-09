3a8032eb7cf91aa3bc6709582a7d791af72149ff
Yuu Ogasawara <y.ogasawara@st-ventures.jp>
2017-06-09 12:04:02 +0900

    Plistに自作オブジェクトを保存し、読み込むテストを追加

--- PlayPropertyListObjCTests/PlayPropertyListObjCTests.m
+++ PlayPropertyListObjCTests/PlayPropertyListObjCTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "Person.h"
 
 @interface PlayPropertyListObjCTests : XCTestCase
 
@@ -24,6 +25,7 @@
     [super tearDown];
 }
 
+#pragma mark Plistへの配列の保存と読み込み
 -(void)testSavePlist{
     NSString *directory = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents"];
     NSString* filePath = [directory stringByAppendingPathComponent:@"data.plist"];
@@ -58,4 +60,100 @@
     }
 }
 
+
+#pragma mark Plistへの自作オブジェクトの保存と読み込み
+//自作オブジェクトを直接Plistに保存することはできないので、NSDataに変換して保存する
+-(void)testSaveObjectToPlist{
+    NSString *directory = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents"];
+    NSString* filePath = [directory stringByAppendingPathComponent:@"data.plist"];
+    
+    NSArray<Person*>* personsArray = [self personsArray];
+    NSMutableArray<NSData*>* array = [NSMutableArray array];
+    
+    for (Person* person in personsArray) {
+        NSData* data = [NSKeyedArchiver archivedDataWithRootObject:person];
+        [array addObject:data];
+    }
+    
+    BOOL successful = [array writeToFile:filePath atomically:YES];
+    XCTAssertTrue(successful);
+}
+
+-(void)testLoadObjectNotNil{
+    NSString *directory = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents"];
+    NSString* filePath = [directory stringByAppendingPathComponent:@"data.plist"];
+    
+    NSArray<Person*>* personsArray = [self personsArray];
+    NSMutableArray<NSData*>* array = [NSMutableArray array];
+    
+    for (Person* person in personsArray) {
+        NSData* data = [NSKeyedArchiver archivedDataWithRootObject:person];
+        [array addObject:data];
+    }
+    
+    [array writeToFile:filePath atomically:YES];
+
+    NSArray<NSData*> *loadedArray = [[NSArray alloc] initWithContentsOfFile:filePath];
+    XCTAssertNotNil(loadedArray);
+}
+
+-(void)testLoadedObjectValid{
+    NSString *directory = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents"];
+    NSString* filePath = [directory stringByAppendingPathComponent:@"data.plist"];
+    
+    NSArray<Person*>* personsArray = [self personsArray];
+    NSMutableArray<NSData*>* array = [NSMutableArray array];
+    
+    for (Person* person in personsArray) {
+        NSData* data = [NSKeyedArchiver archivedDataWithRootObject:person];
+        [array addObject:data];
+    }
+    
+    [array writeToFile:filePath atomically:YES];
+    
+    NSArray<NSData*> *loadedArray = [[NSArray alloc] initWithContentsOfFile:filePath];
+    
+    if (loadedArray) {
+        [loadedArray enumerateObjectsUsingBlock:^(NSData* data, NSUInteger idx, BOOL *stop){
+            //NSDataをモデルクラスに変換
+            Person* person = [NSKeyedUnarchiver unarchiveObjectWithData:data];
+            Person* referencedPerson = personsArray[idx];
+            
+            XCTAssertEqualObjects(person.name, referencedPerson.name);
+            
+            Address* referencedAddress = referencedPerson.address;
+            XCTAssertEqualObjects(person.address.zipCode, referencedAddress.zipCode);
+            XCTAssertEqualObjects(person.address.state, referencedAddress.state);
+            XCTAssertEqualObjects(person.address.city, referencedAddress.city);
+            XCTAssertEqualObjects(person.address.other, referencedAddress.other);
+        }];
+
+    }
+}
+
+-(NSArray<Person*>*)personsArray{
+    Person *tYamada = [[Person alloc] init];
+    tYamada.name = @"山田敏郎";
+    Address* yAddress = [[Address alloc] init];
+    yAddress.zipCode = @"104-0061";
+    yAddress.state = @"東京都";
+    yAddress.city = @"中央区";
+    yAddress.other = @"銀座１丁目";
+    tYamada.address = yAddress;
+    
+    Person* hYamada = [[Person alloc] init];
+    hYamada.name = @"山田ひかり";
+    hYamada.address = yAddress;
+    
+    Person *tanaka = [[Person alloc] init];
+    tanaka.name = @"田中翔太";
+    Address* tAddress = [[Address alloc] init];
+    tAddress.zipCode = @"145-0071";
+    tAddress.state = @"東京都";
+    tAddress.city = @"大田区";
+    tAddress.other = @"田園調布１丁目";
+    tanaka.address = tAddress;
+    
+    return @[tYamada,hYamada,tanaka];
+}
 @end
